92be0440f5e0967b83e76ee3ef713e0dd578de5d
MaxMeyer <mmtrash3@gmail.com>
2019-11-18 18:56:21 +0300

    welcome page

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/indexController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/indexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class indexController {
 
-    @RequestMapping({"", "/", "index.html"})
+    @RequestMapping({"", "/", "/index", "index.html"})
     public String index(){
         return "index";
     }
